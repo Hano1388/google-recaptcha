c2a0a4ed885a8d4c1625ce2c5f0797665bcf1ecf
Hano <hinsul3113@gmail.com>
2019-06-10 22:27:10 -0700

    sent an ajax post request to backend with captcha

--- server.js
+++ server.js
@@ -1,56 +1,45 @@
+const secrets = require("./env");
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
 
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/captcha", (req, res) => {
+app.post("/recaptcha", (req, res) => {
   if (
     req.body.captcha === undefined ||
     req.body.captcha === "" ||
     req.body.captcha === null
   ) {
-    return res.json({
-      success: false,
-      msg: "Please Select captcha"
-    });
+    return res.json({ success: false, msg: "Please Select captcha" });
   }
 
   // Secret Key
-  const secretKey = process.env.RECAPTCHA_SECRET;
+  const { captchaSecret } = secrets;
 
   // Verify URL
-  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req
-    .body.captcha}$remoteip=${req.connection.remoteAddress}`;
+  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${captchaSecret}&response=${req
+    .body.captcha}&remoteip=${req.connection.remoteAddress}`;
 
   // Make request to verify URL
   request(verifyUrl, (err, response, body) => {
     body = JSON.parse(body);
-  });
+    console.log(body);
 
-  // If not successful
-  if (body.success !== undefined && !body.success) {
-    return res.json({
-      success: false,
-      msg: "Failed captcha verification"
-    });
-  }
+    // If not successful
+    if (body.success !== undefined && !body.success) {
+      return res.json({ success: false, msg: "Failed captcha verification" });
+    }
 
-  // If successful
-  return res.json({
-    success: true,
-    msg: "Captcha successfully passed"
+    // If successful
+    return res.json({ success: true, msg: "Captcha successfully passed" });
   });
 });
 
